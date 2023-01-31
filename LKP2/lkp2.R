a70265c21fbf972cca216cd669c2eac5ca091ef4
1nvers <95286008+1nvers@users.noreply.github.com>
2023-01-31 21:58:54 +0700

    update no4

--- LKP2/lkp2.R
+++ LKP2/lkp2.R
@@ -8,16 +8,39 @@ summary(data)
 
 
 #4
-hist(data$LotFrontage, main="test",xlim=c(20,120),breaks = 20)
-hist(data$LotArea, main="test", xlim=c(0,20000), breaks = 20)
-hist(data$GarageArea, main="test", breaks = 40)
-hist(data$SalePrice, main="test")
+salesprice = c()
+salesprice
+for (i in 1:100){
+  salesprice[i] = floor(data$SalePrice[i]/1000)
+  data$SalePrice[i]
+  salesprice[i]
+}
 
-barplot(table(data$MSZoning), xlab= "tahun", ylab="Frekuensi", main="test")
-barplot(table(data$LotShape), xlab= "tahun", ylab="Frekuensi", main="test")
-barplot(table(data$OverallQual), xlab= "tahun", ylab="Frekuensi", main="test")
-barplot(table(data$YearBuilt), xlab= "tahun", ylab="Frekuensi", main="test")
-barplot(table(data$RoofStyle), xlab= "tahun", ylab="Frekuensi", main="test")
+hist(data$LotFrontage, xlab= "Lot Frontage", ylab="Frekuensi",
+     main="Persebaran Lot Frontage",xlim=c(0,120),
+     breaks = 100,col = heat.colors(25))
+hist(data$LotArea, xlab= "Lot Area", ylab="Frekuensi",
+     main="Persebaran Lot Area", xlim=c(0,20000), 
+     breaks = 100, col = heat.colors(40))
+hist(data$GarageArea, xlab= "Garage Area", ylab="Frekuensi",
+     main="Persebaran Garage Area", xlim=c(0,1000),
+     breaks = 100, col = heat.colors(25))
+hist(salesprice, xlab= "Sale Price (in thousands)", ylab="Frekuensi",
+     main="Persebaran Sale Price", xlim=c(0,500),
+     breaks = 100, col = heat.colors(25))
+
+
+barplot(table(data$MSZoning), xlab= "MS Zoning", ylab="Frekuensi", 
+        main="Frekuensi Kemunculan MSZoning",col = topo.colors(10))
+barplot(table(data$LotShape), xlab= "Lot Shape", ylab="Frekuensi", 
+        main="Frekuensi Kemunculan Lot Shape",col = topo.colors(10))
+barplot(table(data$OverallQual), xlab= "Overall Qual", ylab="Frekuensi", 
+        main="Frekuensi Kemunculan OverallQual",col = topo.colors(10))
+barplot(table(data$YearBuilt), xlab= "Year Built", ylab="Frekuensi", 
+        main="Frekuensi Kemunculan YearBuilt",col = topo.colors(120))
+barplot(table(data$RoofStyle), xlab= "Roof Style", ylab="Frekuensi", 
+        main="Frekuensi Kemunculan RoofStyle",col = topo.colors(10))
+
 
 
 #5
