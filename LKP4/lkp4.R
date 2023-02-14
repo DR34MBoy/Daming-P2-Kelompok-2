9e103ebc9ebf804197ccc9d96442a02a871cc544
DR34MBoy <fgh0608@gmail.com>
2023-02-14 14:58:20 +0700

    Update lkp4.R

--- LKP4/lkp4.R
+++ LKP4/lkp4.R
@@ -18,7 +18,6 @@ str(data)
 
 # No. 1 (Cek missing value dan ubah missing value)
 md.pattern(data)
-
 # Unit Price (Numerik)
 data$Unit.price[is.na(data$Unit.price)] <- mean (data$Unit.price, na.rm= TRUE)
 # Quantity (Numerik)
@@ -29,8 +28,6 @@ data$cogs[is.na(data$cogs)] <- mean (data$cogs, na.rm= TRUE)
 data$Rating[is.na(data$Rating)] <- names(sort(-table(data$Rating))[1])
 # gross.income (Numerik)
 data$gross.income[is.na(data$gross.income)] <- mean (data$gross.income, na.rm= TRUE)
-
-x
 
 # No. 2 (Diskretasi)
 ## Equal Width
@@ -47,11 +44,7 @@ ef.rating <- discretize(data$Unit.price, "equalfreq", 5)
 data <- transform(data, total_price = data$Unit.price * data$Quantity * 1.05)
 data <- transform(data, tax_5percent = data$total_price - (data$Unit.price * data$Quantity))
 
-typeof(data$gross.income)
-typeof(data$total_price)
-typeof(data$tax_5percent)
-
-df.norm <- normalize(data[11:12], method = "range", range = c(0, 1))
+data[11:12] <- normalize(data[11:12], method = "range", range = c(0, 1))
 
 # No.4 Integrasi data
 
