e1f4cdf868284a20aa51293fc525531a6c85c182
DR34MBoy <fgh0608@gmail.com>
2023-05-28 17:17:27 +0700

    update ta

--- TA/test.R
+++ TA/test.R
@@ -18,16 +18,16 @@ set.seed(123)
 
 ## Read dataset ##
 gdp <- read.csv('countries.csv')
-## gdp_fix <- gdp[c(3:5, 7:8)]
+gdp_fix <- gdp[c(3:5, 7:8)]
 
 
 ## Split dataset by continent ##
-gdp_Asia <- filter(gdp, Continent == "Asia")
-gdp_Africa <- filter(gdp, Continent == "Africa")
-gdp_NA <- filter(gdp, Continent == "North America")
-gdp_SA <- filter(gdp, Continent == "South America")
-gdp_Europe <- filter(gdp, Continent == "Europe")
-gdp_Oceania <- filter(gdp, Continent == "Oceania")
+gdp_Asia <- filter(gdp_fix, Continent == "Asia")
+gdp_Africa <- filter(gdp_fix, Continent == "Africa")
+gdp_NA <- filter(gdp_fix, Continent == "North America")
+gdp_SA <- filter(gdp_fix, Continent == "South America")
+gdp_Europe <- filter(gdp_fix, Continent == "Europe")
+gdp_Oceania <- filter(gdp_fix, Continent == "Oceania")
 
 
 ## Scatter plot ##
@@ -58,63 +58,63 @@ ggplot(gdp_Oceania, aes(x=Population, y=GDP_per_capita, fill = "blue")) +
 
 ## Extra setup ##
 # Asia #
-gdp_Asia_v2 <- gdp_Asia[,c(3, 5, 8)]
+gdp_Asia_v2 <- gdp_Asia[,c(1, 3, 5)]
 gdp_Asia_v3 <- gdp_Asia_v2[,-1]
 rownames(gdp_Asia_v3) <- gdp_Asia_v2[,1]
 
 # Determine optimal cluster
 gap_stat_Asia <- clusGap(gdp_Asia_v3, FUN = kmeans, nstart = 25,
-                    K.max = 10, B = 50)
+                         K.max = 10, B = 50)
 fviz_gap_stat(gap_stat_Asia)
 
 # Africa #
-gdp_Africa_v2 <- gdp_Africa[,c(3, 5, 8)]
+gdp_Africa_v2 <- gdp_Africa[,c(1, 3, 5)]
 gdp_Africa_v3 <- gdp_Africa_v2[,-1]
 rownames(gdp_Africa_v3) <- gdp_Africa_v2[,1]
 
 # Determine optimal cluster
 gap_stat_Africa <- clusGap(gdp_Africa_v3, FUN = kmeans, nstart = 25,
-                    K.max = 10, B = 50)
+                           K.max = 10, B = 50)
 fviz_gap_stat(gap_stat_Africa)
 
 # North America #
-gdp_NA_v2 <- gdp_NA[,c(3, 5, 8)]
+gdp_NA_v2 <- gdp_NA[,c(1, 3, 5)]
 gdp_NA_v3 <- gdp_NA_v2[,-1]
 rownames(gdp_NA_v3) <- gdp_NA_v2[,1]
 
 # Determine optimal cluster
 gap_stat_NA <- clusGap(gdp_NA_v3, FUN = kmeans, nstart = 25,
-                    K.max = 10, B = 50)
+                       K.max = 10, B = 50)
 fviz_gap_stat(gap_stat_NA)
 
 # South America
-gdp_SA_v2 <- gdp_SA[,c(3, 5, 8)]
+gdp_SA_v2 <- gdp_SA[,c(1, 3, 5)]
 gdp_SA_v3 <- gdp_SA_v2[,-1]
 rownames(gdp_SA_v3) <- gdp_SA_v2[,1]
 
 # Determine optimal cluster 
 gap_stat_SA <- clusGap(gdp_SA_v3, FUN = kmeans, nstart = 25,
-                    K.max = 10, B = 50)
+                       K.max = 10, B = 50)
 fviz_gap_stat(gap_stat_SA)
 
 # Europe #
-gdp_Europe_v2 <- gdp_Europe[,c(3, 5, 8)]
+gdp_Europe_v2 <- gdp_Europe[,c(1, 3, 5)]
 gdp_Europe_v3 <- gdp_Europe_v2[,-1]
 rownames(gdp_Europe_v3) <- gdp_Europe_v2[,1]
 
 # Determine optimal cluster
 gap_stat_Europe <- clusGap(gdp_Europe_v3, FUN = kmeans, nstart = 25,
-                    K.max = 10, B = 50)
+                           K.max = 10, B = 50)
 fviz_gap_stat(gap_stat_Europe)
 
 # Oceania #
-gdp_Oceania_v2 <- gdp_Oceania[,c(3, 5, 8)]
+gdp_Oceania_v2 <- gdp_Oceania[,c(1, 3, 5)]
 gdp_Oceania_v3 <- gdp_Oceania_v2[,-1]
 rownames(gdp_Oceania_v3) <- gdp_Oceania_v2[,1]
 
 # Determine optimal cluster
 gap_stat_Oceania <- clusGap(gdp_Oceania_v3, FUN = kmeans, nstart = 25,
-                    K.max = 10, B = 50)
+                            K.max = 10, B = 50)
 fviz_gap_stat(gap_stat_Oceania)
 
 
